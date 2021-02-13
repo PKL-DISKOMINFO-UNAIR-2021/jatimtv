4be998ee3c5448cf2bf8b92da5b4e5de118319b9
Alvian Yulianto <alvianyulianto07@gmail.com>
2021-02-13 17:35:14 +0700

    recom

--- resources/views/blog/isi_post.blade.php
+++ resources/views/blog/isi_post.blade.php
@@ -33,11 +33,7 @@
                 @foreach($datas ['data'] as $r)
                 <div class="small-img-row">
                     <div class="small-img">
-<<<<<<< HEAD
-                       <img src="{{ $r->gambar }}" alt="{{ $r->gambar }}">
-=======
-                        <img src="{{ $r->gambar }}"/>
->>>>>>> 96eac3763139e1820474d78a2a87a1e77fc5800c
+                       <img src="../{{ $r->gambar }}" alt="{{ $r->gambar }}">
                     </div>
                     <a href="{{ route('blog.isi', $r->slug ) }}"><p>{{ $r->judul }}</p></a>
                 </div>
